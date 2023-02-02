19e005a0ef903feed67a2b0eb2943e69d96da91f
Carys Jones <carys.jones@wolfson.ox.ac.uk>
2023-02-02 14:03:39 +0000

    fix mistake data > output

--- R/dirs.R
+++ R/dirs.R
@@ -10,7 +10,7 @@ dirs$root <- rprojroot::find_rstudio_root_file()
 dirs$R <- file.path(dirs$root, "R")
 dirs$plots <- file.path(dirs$root, "plots")
 dirs$data <- file.path(dirs$root, "data")
-dirs$data <- file.path(dirs$root, "output")
+dirs$output <- file.path(dirs$root, "output")
 for (dir in dirs) {
   if (!dir.exists(dir)) {
     dir.create(dir, recursive = TRUE)
