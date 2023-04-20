b3b279008274cff2f41c045b15205bd5b0177b99
Carys Jones <carys.jones@wolfson.ox.ac.uk>
2023-04-20 14:15:55 +0100

    add raw and output data folders

--- R/dirs.R
+++ R/dirs.R
@@ -9,8 +9,9 @@ dirs <- list()
 dirs$root <- rprojroot::find_rstudio_root_file()
 dirs$R <- file.path(dirs$root, "R")
 dirs$plots <- file.path(dirs$root, "plots")
-dirs$data <- file.path(dirs$root, "data")
-dirs$output <- file.path(dirs$root, "output")
+dirs$data_raw <- file.path(dirs$root, "data_raw")
+dirs$data_output <- file.path(dirs$root, "data_output")
+dirs$model_output <- file.path(dirs$root, "model_output")
 for (dir in dirs) {
   if (!dir.exists(dir)) {
     dir.create(dir, recursive = TRUE)
