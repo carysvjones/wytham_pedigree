60d796c15075339d7c9ec75005fe873cd93cb85b
Carys Jones <carys.jones@wolfson.ox.ac.uk>
2023-04-20 14:31:29 +0100

    edit data folders

--- analysis/pre_prep_data.R
+++ analysis/pre_prep_data.R
@@ -5,14 +5,11 @@ box::use(.. / R / dirs[dirs])
 box::use(clean = ../ R / clean_data)
 box::use(mods = ../ R / for_models)
 
-box::use(readr[read_csv])
-box::use(ggplot2[...])
+box::use(ggplot2)
 box::use(dplyr)
 box::use(moments)
-box::use(kinship2)
 box::use(sf)
 box::use(tidyr)
-box::use(pedantics)
 box::use(magrittr[`%>%`])
 
 # READ IN - Breeding Data -------------------------------------------------
@@ -21,18 +18,18 @@ box::use(magrittr[`%>%`])
 
 #merge 2021 data
 #read in data 1960-2020
-breed <- read.csv(file.path(dirs$data, 'LT_breeding_data_greti_bluti_1960_2020.csv'), na.strings=c("", "NA"))
+breed <- read.csv(file.path(dirs$data_raw, 'LT_breeding_data_greti_bluti_1960_2020.csv'), na.strings=c("", "NA"))
 nrow(breed) #39530
 
 #read in 2021 data
-breed2021 <- read.csv(file.path(dirs$data, 'ebmp_broods_2021.csv'), na.strings=c("", "NA")) %>%
+breed2021 <- read.csv(file.path(dirs$data_raw, 'ebmp_broods_2021.csv'), na.strings=c("", "NA")) %>%
   #add column with year
   dplyr::mutate(year = as.integer(2021))
 nrow(breed2021) #1209
 head(breed2021)
 
 #read in 2022 data 
-breed2022 <- read.csv(file.path(dirs$data, 'ebmp_broods_2022.csv'), na.strings=c("", "NA")) %>%
+breed2022 <- read.csv(file.path(dirs$data_raw, 'ebmp_broods_2022.csv'), na.strings=c("", "NA")) %>%
   #add column with year
   dplyr::mutate(year = as.integer(2022))
 nrow(breed2022) #1248
@@ -41,24 +38,30 @@ nrow(breed2022) #1248
 #bind 2
 breed <- rbind(breed, breed2021, breed2022)
 nrow(breed) #41987
-write.csv(breed, file = file.path(dirs$data,'LT_breeding_data_greti_bluti_1960_2022.csv'), row.names = F)
+
+
+# clean dataset 
+breed <- clean$clean_breeding_data(breed)
+nrow(breed) #38017
+
+write.csv(breed, file = file.path(dirs$data_output,'LT_breeding_data_greti_bluti_1960_2022.csv'), row.names = F)
 
 
 # READ IN - Ringing Data -------------------------------------------------
 
 #get ringing data sets and add together - 
 #first join 2013-2020 data with 2021 and 2022
-ring2 <- read.csv(file.path(dirs$data, 'ebmp_database_ringing_record_export_GT&BT_2013-20.csv'),
+ring2 <- read.csv(file.path(dirs$data_raw, 'ebmp_database_ringing_record_export_GT&BT_2013-20.csv'),
                   na.strings=c("", "NA"))
 
-ring2021 <- read.csv(file.path(dirs$data, 'ebmp_ringing_data_2021.csv'),
+ring2021 <- read.csv(file.path(dirs$data_raw, 'ebmp_ringing_data_2021.csv'),
                   na.strings=c("", "NA"))
 
 nrow(ring2) #51566
 nrow(ring2021) #8197
 
 #read in 2022 data 
-ring2022 <- read.csv(file.path(dirs$data, 'ebmp_ringing_2022.csv'), na.strings=c("", "NA")) 
+ring2022 <- read.csv(file.path(dirs$data_raw, 'ebmp_ringing_2022.csv'), na.strings=c("", "NA")) 
 nrow(ring2022) #8197
 
 
@@ -66,26 +69,22 @@ nrow(ring2022) #8197
 ring2 <- rbind(ring2, ring2021, ring2022)
 nrow(ring2) #67232
 
-#just input 2013-2021 data
-write.csv(ring2, file = file.path(dirs$data, 'ebmp_database_ringing_record_export_GT&BT_2013-22.csv'), row.names = F)
-
 
 # READ IN - Ringing Data up to 2013 --------------------------------------------------
 
 #then also add data 1960-2013
-ring <- read.csv(file.path(dirs$data, 'legacy_ringing_records_GT&BT_up_to_2013.csv'), na.strings=c("", "NA")) %>%
+ring <- read.csv(file.path(dirs$data_raw, 'legacy_ringing_records_GT&BT_up_to_2013.csv'), na.strings=c("", "NA")) %>%
   #clean with function - removes errors and cleans up dataframe
-  clean_ringing_data(.) %>%
+  clean$clean_ringing_data(.) %>%
   #keep only some columns
   dplyr::select(Pnum, age, sex, bto_species_code, bto_ring, yr, nb, retrap)
 nrow(ring) #151785
 
-# READ IN - Ringing data 2013 onwards -------------------------------------
+# Clean Ringing data 2013-2022  -------------------------------------
 
-ring2 <- read.csv(file.path(dirs$data, 'ebmp_database_ringing_record_export_GT&BT_2013-22.csv'),
-                  na.strings=c("", "NA")) %>% 
+ring2 <- ring2 %>% 
   #clean with function - removes errors and cleans up dataframe
-  clean_ringing_data_2(.) %>%
+  clean$clean_ringing_data_2(.) %>%
   dplyr::mutate(Pnum = paste0(Date, '1', Site)) %>%
   #rename columns
   dplyr::rename(bto_ring = Ring,
@@ -107,23 +106,23 @@ ring_all <- rbind(ring, ring2)
 nrow(ring_all) #187067
 
 #save
-write.csv(ring_all, file = file.path(dirs$data, 'ebmp_database_ringing_record_export_GT&BT_all.csv'), row.names = F)
+write.csv(ring_all, file = file.path(dirs$data_output, 'ebmp_database_ringing_record_export_GT&BT_all.csv'), row.names = F)
 
 
 
 
-# ADD SPATIAL DATA ----------------------------------------------
+# SPATIAL DATA ----------------------------------------------
 
 #make combined dataset of all nestbox environment data
 
 #read in nest box data and use some classifcations of habitat
-nestbox <- read.csv(file.path(dirs$data, 'Nest box habitat data.csv'), na.strings=c("", "NA"))
+nestbox <- read.csv(file.path(dirs$data_raw, 'Nest_box_habitat_data.csv'), na.strings=c("", "NA"))
 nrow(nestbox) #1019
 nestbox <- nestbox %>%
   dplyr::select('Box', 'Section', 'x', 'y', 'edge.EDI.', 'altitude.m.', 'northness')
 
 #oak data - not for all boxes.... leave some with NA
-oak <- read.csv(file.path(dirs$data, 'Tit_data.csv'), na.strings=c("", "NA"))
+oak <- read.csv(file.path(dirs$data_raw, 'Tit_oak_data.csv'), na.strings=c("", "NA"))
 nrow(oak) #987
 oak <- oak %>%
   dplyr::select('Box', 'No_trees_75m')
@@ -132,12 +131,11 @@ box_tree <- merge(nestbox, oak, by = 'Box', all.x = T)
 head(box_tree)
 nrow(box_tree) #1019
 
-
   
 # get territory areas ----------------------------------------------
 
 #open wood outline file
-wood.outline <- sf::st_read(file.path(dirs$data, '/maps/perimeter poly with clearings_region.shp'))
+wood.outline <- sf::st_read(file.path(dirs$data_raw, '/maps/perimeter poly with clearings_region.shp'))
 
 #extracting the first polygon because that's the bit of the woods we care about
 #(and the polygons go a bit nuts if you don't)
@@ -152,8 +150,6 @@ breeding.data <- breed %>%
   dplyr::inner_join(., nestbox, by = c('nest.box' = 'Box'))
 nrow(breeding.data) #37577
 
-
-##GREAT TIT ONLY 
 #get subset of just great tits
 breeding.data.G <- subset(breeding.data, Species == 'g' )
 
@@ -211,13 +207,12 @@ GTIT_allyrs_area$area_polygon.G <- as.numeric(GTIT_allyrs_area$area_polygon.G)
 territory <- GTIT_allyrs_area %>%
   dplyr::select('Pnum', 'nest.box', 'area_polygon.G')
 
-
 #merge with other habitat data
 box_tree_terr <- merge(box_tree, territory, by.x = 'Box', by.y = 'nest.box', all.y = T)
-nrow(box_tree_terr) #16872 / 17812
+nrow(box_tree_terr) #17812
 
 #save
-write.csv(box_tree_terr, file = file.path(dirs$data,'Habitat_data_nestboxes.csv'), row.names = F)
+write.csv(box_tree_terr, file = file.path(dirs$data_output,'Habitat_data_nestboxes.csv'), row.names = F)
 
 
 
